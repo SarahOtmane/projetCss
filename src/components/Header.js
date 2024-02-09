e95a58049a8c6bad08c8f4a4f4a33987c8834445
Sarah Otmane <sarahotmane@MacBook-Air-de-Sarah.local>
2024-02-09 12:31:29 +0100

    premiere version du composant header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header({dateAvecJour}){
     return(
         <Stack direction="horizontal" gap={3} className='px-4 py-2'>
             <div className="p-2 text text-uppercase">citations et anniversaires</div>
-            <div className="p-2 ms-auto text text-uppercase">{dateAvecJour}</div>
+            <div className="p-2 ms-auto text text-uppercas">{dateAvecJour}</div>
         </Stack>
     )
 }

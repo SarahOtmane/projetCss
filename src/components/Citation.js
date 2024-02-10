29cfe2672fdb1587236ce7da3b7e4e08fd8892a4
Sarah Otmane <sarahotmane@MacBook-Air-de-Sarah.local>
2024-02-10 10:40:05 +0100

    mise a jour de la progress bar

--- src/components/Citation.js
+++ src/components/Citation.js
@@ -1,16 +1,19 @@
 import Image from 'react-bootstrap/Image';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { ProgressBar } from 'react-bootstrap';
 
 import gif from '../assets/pics/gif.svg';
 
 function Citation(){
 
+    const now = 60;
+
     return(
         <Col xs={6}>
             <Row>
                 <Col xs={12}>
-                    <div className='rose z-0 mx-5 my-4 pb-3 border-2 border-black rounded-4'>
+                    <div className='rose z-0 m-5 pb-3 border-2 border-black rounded-4'>
                     <article className='ms-3 border border-2 border-black rounded-4 z-1 white'>
                         <p className='mx-3 text'>
                             I've missed more than 9,000 shots in my career. I've lost almost 300 games. 
@@ -24,7 +27,7 @@ function Citation(){
             </Row>
             <Row>
                 <Col xs={12}>
-                    <div className='rose z-0 my-2 mx-5 pb-2 border-2 border-black rounded-4'>
+                    <div className='rose z-0 my-2 mx-5 pb-1 border-2 border-black rounded-4'>
                     <figure className='ms-3'>
                         <Image src={gif} className='w-100'/>
                     </figure>
@@ -33,11 +36,7 @@ function Citation(){
             </Row>
             <Row className='mx-5 my-4 justify-content-center'>
                 <Col xs={1}><p className='text1'>01</p></Col>
-                <Col xs={8}>
-                    <div className="progress m-3 mt-5" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                        <div className="progress-bar w-75"></div>
-                    </div>
-                </Col>
+                <Col xs={8}><ProgressBar now={now} label={`${now}%`} visuallyHidden /></Col>
                 <Col xs={1}><p className='text1'>03</p></Col>
             </Row>
         </Col>
